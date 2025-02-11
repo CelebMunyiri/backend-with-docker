8a1ff0c96e7316357fe759266e723d04ace65bd2
David Munyiri <david@nathandigital.com>
2025-02-11 10:26:52 +0300

    added login functionality

--- services/user.service.js
+++ services/user.service.js
@@ -1,6 +1,7 @@
 const {User,Task,Profession} = require('../models')
 const bcrypt = require('bcrypt')
 const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
 
 const createUserService = async(body)=>{
         const user = new User(body)
@@ -88,13 +89,39 @@ const getUsers = async(query)=>{
 }
 
 
+const userLogin = async(emailOrphone,password)=>{
+    try{
+
+        const user = await User.findOne({ $or: [{ email: emailOrphone }, { phone: emailOrphone }] }).select('+password');
+
+        const JWT_SECRET = process.env.JWT_SECTRET;
+
+        let correctUser = await bcrypt.compare(password, password)
+
+        const token = await jwt.sign({
+            id: user._id,
+            email: user.email,
+            name: user.name,
+            phone: user.phone
+        },JWT_SECRET,
+    { expiresIn: '1h' });
+
+ resizeBy.status(200).json(token)
+        
+    }catch(e){
+        throw new Error(`Error logging in user: ${e.message}`);
+    }
+}
+
+
 
 module.exports = {
     createUserService,
     updateUser,
     softDelete,
     updatePassword,
-    getUsers
+    getUsers,
+    userLogin
  
 };
 
